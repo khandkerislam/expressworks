0bbd3f8a710a5bbfe5e6671163c1c2b2b11cf841
khandkerislam <khandker.islam46@gmail.com>
2020-04-06 23:03:20 -0400

    second exercise

--- program.js
+++ program.js
@@ -1,5 +1,4 @@
 const express = require('express');
 const app = express();
-app.get('/home',function(req,res){
-    res.send('Hello World!');
-}).listen(Number(process.argv[2]));
+app.use(express.static(process.argv[3] || path.join(__dirname, 'public')))
+.listen(Number(process.argv[2]));
