d3c4b0e72dfce18d75fc93fe659bc5220b77cee9
khandkerislam <khandker.islam46@gmail.com>
2020-04-07 20:38:18 -0400

    finished fourth exercise

---
+++ forms.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const bodyparser = require('body-parser');
+
+const app = express();
+
+app.use(bodyparser.urlencoded({extended: false}))
+
+app.post('/form', (req,res) => {
+    res.send(req.body.str.split('').reverse().join(''));
+}).listen(Number(process.argv[2]));
